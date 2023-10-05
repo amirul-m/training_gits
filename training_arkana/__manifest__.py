918a228032cd869f9a53ea9581090a9f752391df
Amirul <muh.amirul.muk@gmail.com>
2023-10-05 11:36:05 +0700

    [ADD] training_arkana: day 2

---
+++ training_arkana/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Training Arkana",
+    "version": "16.0.1.0.0",
+    "author": "Amirul M.",
+    "category": "Extra Tools",
+    "license": "LGPL-3",
+    "website": "https://arkana.co.id",
+    "depends": ["contacts"],
+    "data": [
+        'views/res_partner_view.xml'
+    ],
+    "auto_install": False,
+    "installable": True,
+}
