996cb3ecebd1170ad90f88ed74a118d5e15879c2
Amirul <muh.amirul.muk@gmail.com>
2023-10-12 10:53:41 +0700

    [ADD] training_arkana_day4

---
+++ training_arkana_day4/models/training_class.py
@@ -0,0 +1,8 @@
+from odoo import models, fields, api
+
+
+class TrainingClass(models.Model):
+    _inherit = 'training.class'
+    
+    name = fields.Char(string='Class Name')
+    max_pertemuan = fields.Integer(string='Maximal Pertemuan')
